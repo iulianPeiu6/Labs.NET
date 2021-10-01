7b0198ae14768a81b2c22dc85874e341dd84ae9c
iulianPeiuOsf <iulian.peiu@osf.digital>
2021-10-01 11:56:08 +0300

    Lab1- Start Implementation for Second Part of the Requirements for lab1

---
+++ lab1/Utility/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Utility.Extensions
+{
+    public static class StringExtensions
+    {
+        public static int GetNumberOfWords(this string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
