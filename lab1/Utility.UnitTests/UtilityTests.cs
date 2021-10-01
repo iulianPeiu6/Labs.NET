525ec8a04ad7d564b5de40919951fc6fb27722cb
roxana413 <73295914+roxana413@users.noreply.github.com>
2021-10-01 18:35:05 +0300

    branch test

---
+++ lab1/Utility.UnitTests/UtilityTests.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using Utility.Extensions;
+
+namespace Utility.UnitTests
+{
+    public class UtilityTests
+    {
+        private string text;
+        [SetUp]
+        public void Setup()
+        {
+            text = "Ana are mere!";
+        }
+
+        [Test]
+        public void Given_AString_When_GetNumberOfWordsIsCalled_Then_ReturnsNumberOfWords()
+        {
+            // arrange
+            var expectedNumberOfWords = 3;
+            // act
+            var actualNumberOfWords = text.GetNumberOfWords();
+            // assert
+
+        }
+    }
+}
