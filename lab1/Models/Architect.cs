881fea0a36ba09063b22d711cde7233be8b80e0b
iulianPeiuOsf <iulian.peiu@osf.digital>
2021-10-01 11:52:32 +0300

    Lab1 - Init Solution, Implement First Part of the Requirements for lab1
    Initiate Solution;
    Create Employee Abstract Class
    Create Manager Class;
    Create Architect Class;

---
+++ lab1/Models/Architect.cs
@@ -0,0 +1,12 @@
+﻿using Models.Abstracts;
+
+namespace Models
+{
+    public class Architect : Employee
+    {
+        public override string Salutation()
+        {
+            return $"Hello Architect { GetFullName() }";
+        }
+    }
+}
---
+++ lab1/Models/Manager.cs
@@ -0,0 +1,12 @@
+﻿using Models.Abstracts;
+
+namespace Models
+{
+    public class Manager : Employee
+    {
+        public override string Salutation()
+        {
+            return $"Hello Manager { GetFullName() }";
+        }
+    }
+}
