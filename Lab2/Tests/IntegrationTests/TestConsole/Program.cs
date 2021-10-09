d13e57841981dd45601fa0d01b7ec7bdefb8996e
Iulian Peiu <iulianpeiu6@gmail.com>
2021-10-09 16:04:38 +0300

    Lab2 - Implement Weather Data Processor

---
+++ Lab2/Tests/IntegrationTests/TestConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DataProcessor;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunWeatherDataProcessorTest();
+        }
+
+        public static void RunWeatherDataProcessorTest()
+        {
+            var testFile = @"..\..\..\..\..\DataTest\weather.dat";
+
+            var processor = new WeatherDataProcessor();
+            var records = processor.ReadRecords(testFile);
+
+            foreach (var record in records)
+            {
+                Console.WriteLine($"{ record }");
+            }
+
+            var recordWithSmallestTemperatureSpread = processor.GetRecordWithSmallestTemperatureSpread(records);
+            Console.WriteLine(recordWithSmallestTemperatureSpread);
+        }
+    }
+}
