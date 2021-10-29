1a3d0723f01c0f95516576944935cafca2f6e646
Razvan-Daniel Gheorghita <razvan.gheorghita@info.uaic.ro>
2021-10-29 13:01:29 +0300

    Lab5 - Refactored V3

---
+++ Lab5/INET.Lab05.ISP/INET.Lab05.ISP/RefactoredV3/Abstractions/IGetBonus.cs
@@ -0,0 +1,7 @@
+﻿namespace INET.Lab05.ISP.RefactoredV3.Abstractions
+{
+    interface IGetBonus
+    {
+        decimal GetBonus();
+    }
+}
---
+++ Lab5/INET.Lab05.ISP/INET.Lab05.ISP/RefactoredV3/CalculateVolunteerWage.cs
@@ -0,0 +1,12 @@
+﻿using INET.Lab05.ISP.RefactoredV3.Abstractions;
+
+namespace INET.Lab05.ISP.RefactoredV3
+{
+    class CalculateVolunteerWage : IBenefits
+    {
+        public string Benefits()
+        {
+            return "Tickets to Avatar 2 premiere";
+        }
+    }
+}
